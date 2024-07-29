d3e6e7a37ae6050cb868dc74bbc1d9d0b23dc9f5
Luis Augusto de Petta Didonato <125621812+gutodidonato@users.noreply.github.com>
2024-07-29 20:42:45 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,54 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+restaurante = {
+    1: {"nome": "Restaurante da tia Teteia", "status": "ativo"},
+    2: {"nome": "Cantinho do Sabor", "status": "ativo"},
+    3: {"nome": "Sabor Familiar", "status": "ativo"},
+    4: {"nome": "Rancho da Vovó", "status": "ativo"},
+    5: {"nome": "Sabor Caseiro", "status": "ativo"},
+}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/")
+def read_root():
+    return {"message": "Bem-vindo ao API de Restaurantes"}
+
+@app.get("/restaurante/{id_restaurante}")
+def pegar_restaurante(id_restaurante: int):
+    if id_restaurante in restaurante:
+        return restaurante[id_restaurante]
+    else:
+        return {"erro": f"Não existe um Restaurante com o ID {id_restaurante}"}
+    
+
+@app.get("/todos-restaurantes/")
+def pegar_restaurante():
+    return restaurante
+
+
+@app.post("/inserir-restaurante/{nome_restaurante}-{status}")
+def inserir_restaurante(nome_restaurante: str, status: str):
+    id_restaurante = len(restaurante) + 1
+    restaurante[id_restaurante] = {"nome": nome_restaurante, "status": status}
+    return f"{nome_restaurante} criado com sucesso, seus status é {status}"
+
+
+
+@app.put("/atualizar-restaurante/{id_restaurante}-{nome_restaurante}-{status}")
+def atualizar_restaurante(id_restaurante: int, nome_restaurante: str, status: str):
+    print(restaurante)
+    if not restaurante[id_restaurante]:
+        return "Não existe esse restaurante"
+    else:
+        restaurante[id_restaurante] = {"nome": nome_restaurante, "status": status}
+        return "Restaurante atualizado com sucesso"        
+
+
+@app.delete("/deletar-restaurante/{id_restaurante}")
+def deletar_restaurante(id_restaurante: int):
+    if id_restaurante in restaurante:
+        del restaurante[id_restaurante]
+        return {"Restaurante deletado "}
+    else:
+        return {"erro": f"Não existe um Restaurante com o ID {id_restaurante}"}
+
